885a06d10103435ea6c51bd6de8d218a195fcfbe
yujinkim1 <yujin7621@me.com>
2024-06-13 22:37:21 +0900

    docs: 문서화 주석 추가
    - setColor 메서드를 setTextColor 메서드로 변경
    
    - setFont 메서드를 setTextFont 메서드로 변경

--- Sources/SenseKit/Extensions/UIKit/UILabel+Extension.swift
+++ Sources/SenseKit/Extensions/UIKit/UILabel+Extension.swift
@@ -6,7 +6,13 @@
 import UIKit
 
 public extension UILabel {
-    func setColor(to color: UIColor?) {
+    /// `UILabel`의 텍스트 색상을 설정합니다.
+    ///
+    /// ```
+    /// let label = UILabel()
+    /// label.setFont(to: UIColor())
+    /// ```
+    func setTextColor(to color: UIColor?) {
         if let text = self.text {
             let nsRange = NSRange(location: 0, length: text.count)
             let color = color as Any
@@ -18,7 +24,13 @@ public extension UILabel {
         }
     }
     
-    func setFont(to font: UIFont) {
+    /// `UILabel`의 텍스트 폰트를 설정합니다.
+    ///
+    /// ```
+    /// let label = UILabel()
+    /// label.setFont(to: UIFont())
+    /// ```
+    func setTextFont(to font: UIFont) {
         if let text = self.text {
             let nsRange = NSRange(location: 0, length: text.count)
             
@@ -29,6 +41,12 @@ public extension UILabel {
         }
     }
     
+    /// `UILabel`의 전체 텍스트 행 간격을 설정합니다.
+    ///
+    /// ```
+    /// let label = UILabel()
+    /// label.setLineSpacing(to: 13)
+    /// ```
     func setLineSpacing(to spacing: CGFloat) {
         if let text = self.text {
             let style = NSMutableParagraphStyle()
@@ -43,6 +61,12 @@ public extension UILabel {
         }
     }
     
+    /// `UILabel`의 텍스트 한 행의 높이를 설정합니다.
+    ///
+    /// ```
+    /// let label = UILabel()
+    /// label.setLineHeight(to: 13)
+    /// ```
     func setLineHeight(to height: CGFloat) {
         if let text = self.text {
             let style = NSMutableParagraphStyle()
